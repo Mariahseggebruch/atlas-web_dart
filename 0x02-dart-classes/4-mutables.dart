5f0a99ee2b4dd05c6a95f9f31e2ca1aac95606b8
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-10-08 13:51:36 -0500

    task 4

--- 0x02-dart-classes/4-mutables.dart
+++ 0x02-dart-classes/4-mutables.dart
@@ -1,22 +1,45 @@
 class Password {
-  String? _password;
+  // Private property to store password
+  late String _password;
 
-  Password({String? password}) : _password = password;
-
-  bool isValid() {
-    if (_password == null) {
-      return false;
-    }
-    bool hasUppercase = _password!.contains(new RegExp(r'[A-Z]'));
-    bool hasLowercase = _password!.contains(new RegExp(r'[a-z]'));
-    bool hasDigits = _password!.contains(new RegExp(r'\d'));
-    bool hasValidLength = _password!.length >= 8 && _password!.length <= 16;
-
-    return hasUppercase && hasLowercase && hasDigits && hasValidLength;
+  // Constructor to initialize password
+  Password({required String password}) {
+    this._password = password;
   }
 
+  // Getter for password
+  String get password => _password;
+
+  // Setter for password
+  set password(String value) {
+    _password = value;
+  }
+
+  // Method to validate password based on specified criteria
+  bool isValid() {
+    // Check if password length is between 8 and 16 characters
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    // Check if password contains at least one uppercase letter
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+    // Check if password contains at least one lowercase letter
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+    // Check if password contains at least one digit
+    if (!_password.contains(RegExp(r'\d'))) {
+      return false;
+    }
+    // If all conditions are satisfied, return true
+    return true;
+  }
+
+  // Override toString method to provide custom string representation
   @override
   String toString() {
-    return "Your Password is: $_password";
+    return 'Your Password is: $_password';
   }
 }
